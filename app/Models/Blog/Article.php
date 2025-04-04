a685606050a97f3e951fd1aed6127d82b54e0533
nteper <nteper@codemastersintl.com>
2025-04-04 22:09:28 +0300

    Article Model

--- app/Models/Blog/Article.php
+++ app/Models/Blog/Article.php
@@ -6,11 +6,94 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * 
+ *
+ * @property string $id
+ * @property string $title
+ * @property string $brief
+ * @property string $fullText
+ * @property \Illuminate\Support\Carbon $createdAt
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property int|null $author
+ * @method static \Illuminate\Database\Eloquent\Builder|Article newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Article newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Article query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereBrief($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereFullText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereTitle($value)
+ * @mixin \Eloquent
+ */
 class Article extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'brief',
+        'fullText'
+    ];
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthorId(): ?int
+    {
+        return $this->author;
+    }
+
+    public function setAuthorId(int $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getBrief(): ?string
+    {
+        return $this->brief;
+    }
+
+    public function setBrief(string $brief): static
+    {
+        $this->brief = $brief;
+
+        return $this;
+    }
+
+    public function getFullText(): ?string
+    {
+        return $this->fullText;
+    }
+
+    public function setFullText(string $text): static
+    {
+        $this->fullText = $text;
+
+        return $this;
+    }
 
     public function newUniqueId(): string
     {
